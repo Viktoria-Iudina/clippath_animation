b3540511d4ef9b33a5d0e0777bbcda9c78efed2b
Viktoria-Iudina <jajudinaja@gmail.com>
2022-04-17 11:26:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+        const tl = gsap.timeline({paused: true})
+        const container = document.querySelector('.container')
+
+        tl.to(container, {clipPath: 'polygon(0 0, 100% 0%, 100% 100%, 0 100%)'})
+        tl.to(container, {clipPath: 'polygon(100% 0, 100% 0, 100% 100%, 100% 100%)'}, '+=1')
+        tl.to('h1', {color: 'white'})
+        tl.to('.container2', {clipPath: 'polygon(0 0, 100% 0%, 100% 100%, 0 100%)'})
+
+        container.addEventListener('mouseover', () => {
+            tl.play()
+        })

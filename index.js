d872861520bf48da99c272c7233f4048575d0f66
Viktoria-Iudina <jajudinaja@gmail.com>
2022-04-18 10:20:30 +0200

    Custom cursor

--- index.js
+++ index.js
@@ -9,4 +9,15 @@
         container.addEventListener('mouseover', () => {
             tl.play()
         })
-        
+
+
+// Custom cursor
+const cursorRounded = document.querySelector('.rounded');
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+
+  cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`; 
+}
+window.addEventListener('mousemove', moveCursor)
